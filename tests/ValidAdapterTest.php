30e2b1305b54c0592aa8866a4e35a2a9e7b68105
Zing <zingimmick@outlook.com>
2025-08-16 01:14:33 +0800

    Update ValidAdapterTest.php

--- tests/ValidAdapterTest.php
+++ tests/ValidAdapterTest.php
@@ -65,7 +65,7 @@ class ValidAdapterTest extends TestCase
     protected function tearDown(): void
     {
         parent::tearDown();
-
+if ((string) getenv('MOCK') === 'false')
         $this->ossAdapter->deleteDirectory('fixture');
     }
 
